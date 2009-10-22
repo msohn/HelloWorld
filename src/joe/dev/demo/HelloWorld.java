d97f5a2e8d7f31c4c2ba13be6d56d54292498e1a
Joe Developer <joe@jdev.org>
2009-10-22 22:42:34 +0200

    Implemented hello

---
+++ src/joe/dev/demo/HelloWorld.java
@@ -0,0 +1,12 @@
+package joe.dev.demo;
+
+public class HelloWorld {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+	System.out.println("Hello world!");
+    }
+
+}
