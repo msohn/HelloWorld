c0016a4501f6845adad3ade7d704de10657dd86b
Joe Developer <joe@jdev.org>
2009-10-22 22:42:34 +0200

    Implemented hello

--- src/joe/dev/demo/HelloWorld.java
+++ src/joe/dev/demo/HelloWorld.java
@@ -3,6 +3,7 @@ package joe.dev.demo;
 public class HelloWorld {
 
     /**
+<<<<<<< Upstream, based on origin/master
      * This is the Hello world main method
      * @param args
      */
@@ -10,4 +11,12 @@ public class HelloWorld {
     	// say hello git world test5
     	System.out.println("Hello Git world !");
     }
+=======
+     * @param args
+     */
+    public static void main(String[] args) {
+	System.out.println("Hello world!");
+    }
+
+>>>>>>> 9b51170 Implemented hello
 }
