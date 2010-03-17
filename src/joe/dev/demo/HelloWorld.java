e6b5865ff6e94dd3ee2427fde3c2f23c668200cd
William Shakespeare <will@from.the.past>
2010-03-17 16:41:34 +0100

    Improve greeting
    Signed-off-by: William Shakespeare <will@from.the.past>

--- src/joe/dev/demo/HelloWorld.java
+++ src/joe/dev/demo/HelloWorld.java
@@ -15,7 +15,7 @@ public class HelloWorld {
      * @param args
      */
     public static void main(String[] args) {
-	System.out.println("Hello world!");
+    	System.out.println("Hello Git world!");
     }
 
 >>>>>>> 9b51170 Implemented hello
