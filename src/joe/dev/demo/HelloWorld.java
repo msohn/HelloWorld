adfdbfd21e0b15ff1e3d93a5ea6a25bd4cceb9c5
Matthias Sohn <matthias.sohn@sap.com>
2010-05-19 01:05:06 +0200

    2

--- src/joe/dev/demo/HelloWorld.java
+++ src/joe/dev/demo/HelloWorld.java
@@ -6,7 +6,7 @@ public class HelloWorld {
      * @param args
      */
     public static void main(String[] args) {
-    	System.out.println("Hello Git world!");
+    	System.out.println("Hello Git world2!");
     }
 
 }
