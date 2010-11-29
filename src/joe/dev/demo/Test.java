5cb2325152dc7569e6b1961f2580a139b8cfc004
Matthias Sohn <matthias.sohn@sap.com>
2010-11-29 23:48:47 +0100

    Added Test class

--- src/joe/dev/demo/Test.java
+++ src/joe/dev/demo/Test.java
@@ -1,8 +1,12 @@
 package joe.dev.demo;
 
 public class Test {
+<<<<<<< Upstream, based on origin/master
 	public static void test() {
 		System.out.println("this class should do something");
 		//test1-13
 	}
+=======
+
+>>>>>>> 73308c3 Added Test class
 }
