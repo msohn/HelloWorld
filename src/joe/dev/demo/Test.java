85279e0705acc4cf982c46737f027dfe99e4c9b8
Matthias Sohn <matthias.sohn@sap.com>
2010-11-30 15:56:54 +0100

    Implement a method and Test class

--- src/joe/dev/demo/Test.java
+++ src/joe/dev/demo/Test.java
@@ -2,6 +2,7 @@ package joe.dev.demo;
 
 public class Test {
 <<<<<<< Upstream, based on origin/master
+<<<<<<< Upstream, based on origin/master
 	public static void test() {
 		System.out.println("this class should do something");
 		//test1-13
@@ -9,4 +10,9 @@ public class Test {
 =======
 
 >>>>>>> 73308c3 Added Test class
+=======
+	public static void test() {
+		System.out.println("this class should do something");
+	}
+>>>>>>> 28745c9 Implement a method and Test class
 }
