d2f6c170c8ee35d4c2b377c2ce3882c54b11bf35
Matthias Sohn <matthias.sohn@sap.com>
2013-07-01 01:10:54 +0200

    comment untracked 2

--- src/hello/Untracked.java
+++ src/hello/Untracked.java
@@ -1,8 +1,12 @@
 package hello;
 
 /**
+<<<<<<< Upstream, based on origin/master
  * @author d029788
  *ads
+=======
+ * comment
+>>>>>>> c79d9b5 comment untracked 2
  */
 public class Untracked {
 
