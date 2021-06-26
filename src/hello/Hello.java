7d760c46ae039a5e26f6159414164847573da8ef
Matthias Sohn <matthias.sohn@sap.com>
2021-06-26 20:19:22 +0200

    modify Hello

--- src/hello/Hello.java
+++ src/hello/Hello.java
@@ -3,7 +3,7 @@ package hello;
 public class Hello {
 
 	public static void main(String[] args) {
-		System.out.println("Hello world of git flow");
+		System.out.println("Hello world of git modified flow");
 	}
 
 }
